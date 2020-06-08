84de22c4979497f039d3e3baf43e36fb99515c1d
Jang Wonbeom <jtiger958@gmail.com>
2020-06-09 04:23:29 +0900

    add blank in signup

--- src/Layout/SignUp/SignUpInfoPanel.java
+++ src/Layout/SignUp/SignUpInfoPanel.java
@@ -6,10 +6,10 @@ import java.awt.*;
 public class SignUpInfoPanel extends JPanel {
     public SignUpInfoPanel() {
         setLayout(new GridLayout(5, 1));
-        add(new JLabel("username"));
-        add(new JLabel("password"));
-        add(new JLabel("email"));
-        add(new JLabel("phone number"));
-        add(new JLabel("name"));
+        add(new JLabel("  username  "));
+        add(new JLabel("  password  "));
+        add(new JLabel("  email  "));
+        add(new JLabel("  phone number  "));
+        add(new JLabel("  name  "));
     }
 }
