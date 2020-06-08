23e75231ead01580e2fd43f96758f1aea828a042
Jang Wonbeom <jtiger958@gmail.com>
2020-06-09 04:26:50 +0900

    change SignUpFrame close action

--- src/Layout/SignUpFrame.java
+++ src/Layout/SignUpFrame.java
@@ -15,7 +15,7 @@ public class SignUpFrame extends JFrame implements Observer {
         add(signUpPanel);
 
         setSize(500, 200);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
 
