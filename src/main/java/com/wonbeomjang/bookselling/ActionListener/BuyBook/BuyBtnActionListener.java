4b4f7d7561aa93e294a501f3ac64be397fb912ae
Jang Wonbeom <jtiger958@gmail.com>
2020-06-24 17:49:43 +0900

    add alert in buybook

--- src/main/java/com/wonbeomjang/bookselling/ActionListener/BuyBook/BuyBtnActionListener.java
+++ src/main/java/com/wonbeomjang/bookselling/ActionListener/BuyBook/BuyBtnActionListener.java
@@ -2,11 +2,13 @@ package com.wonbeomjang.bookselling.ActionListener.BuyBook;
 
 import com.wonbeomjang.bookselling.DataUtils.Book.Book;
 import com.wonbeomjang.bookselling.DataUtils.User.User;
+import com.wonbeomjang.bookselling.DataUtils.User.UserState;
 import com.wonbeomjang.bookselling.Layout.BookListUp.BookOnSaleLabelPanel;
 import com.wonbeomjang.bookselling.Layout.Interface.BuyBook;
 import com.wonbeomjang.bookselling.Layout.Interface.SearchUser;
 import com.wonbeomjang.bookselling.Layout.SearchBook.SearchBookPanel;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -34,6 +36,12 @@ public class BuyBtnActionListener implements ActionListener {
             if(panel.isChecked()) {
                 Book book = panel.getBook();
                 User seller = searchUser.search(book.getOwner())[0];
+                if(seller.getUserState().equals(UserState.Deactivate)) {
+                    JOptionPane.showMessageDialog(null,seller.getUsername() + "은 deactivate된 사용자입니다.");
+                    panel.remove(0);
+                    panel.add(new JLabel(" "), 0);
+                    panel.revalidate();
+                }
                 buyBook.buyBook(seller, systemUser, book);
             }
         }
--- src/main/java/com/wonbeomjang/bookselling/DataUtils/Book/BuyBook.java
+++ src/main/java/com/wonbeomjang/bookselling/DataUtils/Book/BuyBook.java
@@ -1,6 +1,7 @@
 package com.wonbeomjang.bookselling.DataUtils.Book;
 
 import com.wonbeomjang.bookselling.DataUtils.User.User;
+import com.wonbeomjang.bookselling.DataUtils.User.UserState;
 import com.wonbeomjang.bookselling.Layout.Interface.SearchBook;
 import com.wonbeomjang.bookselling.Layout.Interface.SearchUser;
 
@@ -8,6 +9,9 @@ public class BuyBook extends com.wonbeomjang.bookselling.Layout.Interface.BuyBoo
     @Override
     public boolean buyBook(User seller, User buyer, Book book) {
         if(book.getOwner().equals(buyer.getUsername()))
+            return false;
+
+        if (seller.getUserState().equals(UserState.Deactivate))
             return false;
 
         book.setOwner(buyer.getUsername());
